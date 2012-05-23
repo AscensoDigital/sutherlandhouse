5fdf0d8cab99bed5492800c14f8d8481d7843c12
claudio <claudio@claudio-usm>
2012-05-23 12:05:44 -0400

    se agrego certificado de calidad tripadvisor al layout de las paginas

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -16,7 +16,10 @@
 <div id="logo"><?php echo link_image_to('logo_vertical.png','@hotel_homepage', array('alt' => __('Logo Hotel Boutique Sutherland House'), 'size' => '420x105'))?></div>
 <div id="idioma"><?php include_partial('language/language')?></div>
 <?php include_slot('galeria')?>
-<div id="info">
+<div id="info"><div id="TA_certificateOfExcellence651" class="TA_certificateOfExcellence">
+<ul id="YOhCi2a3K" class="TA_links 60CbIyl">
+<li id="tRVG17zwj" class="FXLTGo"><a href=http://www.tripadvisor.es/Hotel_Review-g294306-d1957998-Reviews-Hotel_Boutique_Sutherland_House-Valparaiso_Valparaiso_Region.html>Hotel Boutique Sutherland House</a></li>
+</ul></div>
 <?php echo link_image_to('reservas-es.jpg', '@reserva', array('alt' => __('Reservas'), 'title' => __('Reservas'), 'size' => '192x30'))?>
 <?php echo link_image_to('mapa-es.jpg', '@hotel_ubicacion', array('alt' => __('Mapa'), 'title' => __('Mapa'), 'size' => '192x86'))?>
 <?php echo link_image_to('new_year_2012_hotel_sutherland_house.jpg', '@hotel_new_year', array('alt' => __('New Year').' '.date('Y')+1, 'title' => __('New Year').' '.date('Y')+1, 'size' => '192x86'))?>  
@@ -57,6 +60,7 @@
 .eq(prevIndex).fadeTo('fast',onMouseOutOpacity).end()
 .eq(nextIndex).fadeTo('fast',1.0);},onPageTransitionOut:function(callback){this.fadeTo('fast',0.0,callback);},onPageTransitionIn:function(){this.fadeTo('fast',1.0);}});function pageload(hash){if(hash){$.galleriffic.gotoImage(hash);}else{gallery.gotoIndex(0);}}
 $.historyInit(pageload,"advanced.html");$("a[rel='history']").live('click',function(e){if(e.button!=0)return true;var hash=this.href;hash=hash.replace(/^.*#/,'');$.historyLoad(hash);return false;});});</script>
+<script src="http://www.jscache.com/wejs?wtype=certificateOfExcellence&amp;uniq=651&amp;locationId=1957998&amp;lang=<?php echo $sf_params->get('sf_culture')?>&amp;year=2012"></script>
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js">{lang: 'es-419'}</script>
 <script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-16729802-1']);_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script> 
 </body>
