a0f69cad557edceeb6386ed6a25ed0b44850c458
claudiocorvalan <corvalanclaudio7@gmail.com>
2017-05-12 15:24:56 -0300

    se agrega bundle para deploy de proyecto

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Hpatoio\DeployBundle\DeployBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
